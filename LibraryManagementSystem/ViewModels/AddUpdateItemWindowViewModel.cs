2f4c6a2d542c65057d5ed2fb596597a1b4df7687
ItayTzafrirVeev <117359465+ItayTzafrirVeev@users.noreply.github.com>
2024-08-04 16:35:01 +0300

    wip add update item

--- LibraryManagementSystem/ViewModels/AddUpdateItemWindowViewModel.cs
+++ LibraryManagementSystem/ViewModels/AddUpdateItemWindowViewModel.cs
@@ -80,19 +80,88 @@ namespace LibraryManagementSystem.ViewModels
             switch (_selectedItemType)
             {
                 case ItemType.Book:
-                    if (_item is not Book) _item = new Book();
+                    if (_item is not Book)
+                    {
+                        var book = _item as Book ?? new Book();
+                        _item = new Book
+                        {
+                            Title = _item.Title,
+                            Author = book.Author,
+                            Genre = book.Genre,
+                            PageCount = book.PageCount,
+                            Language = book.Language,
+                            Format = book.Format,
+                            Dimensions = book.Dimensions,
+                            Series = book.Series,
+                            Edition = book.Edition,
+                            Keywords = book.Keywords
+                        };
+                    }
                     break;
                 case ItemType.CD:
-                    if (_item is not CD) _item = new CD();
+                    if (_item is not CD)
+                    {
+                        var cd = _item as CD ?? new CD();
+                        _item = new CD
+                        {
+                            Title = _item.Title,
+                            Artist = cd.Artist,
+                            Genre = cd.Genre,
+                            Duration = cd.Duration,
+                            TrackCount = cd.TrackCount,
+                            Label = cd.Label,
+                            ReleaseDate = cd.ReleaseDate,
+                            Tracks = cd.Tracks
+                        };
+                    }
                     break;
                 case ItemType.EBook:
-                    if (_item is not EBook) _item = new EBook();
+                    if (_item is not EBook)
+                    {
+                        var ebook = _item as EBook ?? new EBook();
+                        _item = new EBook
+                        {
+                            Title = _item.Title,
+                            Author = ebook.Author,
+                            FileFormat = ebook.FileFormat,
+                            FileSize = ebook.FileSize,
+                            DownloadLink = ebook.DownloadLink,
+                            Keywords = ebook.Keywords
+                        };
+                    }
                     break;
                 case ItemType.DVD:
-                    if (_item is not DVD) _item = new DVD();
+                    if (_item is not DVD)
+                    {
+                        var dvd = _item as DVD ?? new DVD();
+                        _item = new DVD
+                        {
+                            Title = _item.Title,
+                            Director = dvd.Director,
+                            Genre = dvd.Genre,
+                            Duration = dvd.Duration,
+                            Language = dvd.Language,
+                            Studio = dvd.Studio,
+                            ReleaseDate = dvd.ReleaseDate,
+                            Subtitles = dvd.Subtitles,
+                            Cast = dvd.Cast
+                        };
+                    }
                     break;
                 case ItemType.Magazine:
-                    if (_item is not Magazine) _item = new Magazine();
+                    if (_item is not Magazine)
+                    {
+                        var magazine = _item as Magazine ?? new Magazine();
+                        _item = new Magazine
+                        {
+                            Title = _item.Title,
+                            Editor = magazine.Editor,
+                            IssueNumber = magazine.IssueNumber,
+                            Genre = magazine.Genre,
+                            Frequency = magazine.Frequency,
+                            Articles = magazine.Articles
+                        };
+                    }
                     break;
                 default:
                     _item = new Item();
